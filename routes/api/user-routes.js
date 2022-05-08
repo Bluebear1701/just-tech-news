6c88e1ed02afff2b0997f02ae0175397e50db50d
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-05-07 20:52:46 -0500

    add hashing and compare to validate password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -36,19 +36,38 @@ router.get('/:id', (req, res) => {
 });
 
 //POST /api/users
-router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
+//   User.create({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password
+//   })
+//     .then(dbUserData => res.json(dbUserData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
@@ -60,7 +79,7 @@ router.put('/:id', (req, res) => {
     individualHooks: true,
 
     where: {
-      
+
       id: req.params.id
     }
   })
