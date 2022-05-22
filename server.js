7c398388d886635a4e242de4354eda85941ee166
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-05-22 16:17:51 -0500

    unable t clear error

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//import modules 
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -6,7 +5,6 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
