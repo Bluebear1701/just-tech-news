0a0c1d0ea38368846c93ced076fdeee239886860
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-05-22 14:58:13 -0500

    adding comments

--- models/index.js
+++ models/index.js
@@ -1,4 +1,19 @@
 const User = require('./User');
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User };
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
